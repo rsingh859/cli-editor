ac51aad5ba2dc15e789ba6b4cf08a8a2b7aefb8d
rsingh859 <singh.ritesh2806@gmail.com>
2023-11-22 22:31:01 +0530

    Added Markdown editor

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,12 @@
 import "bulmaswatch/superhero/bulmaswatch.min.css";
 import "./App.css";
 import CodeCell from "./components/code-cell";
+import TextEditor from "./components/text-editor";
 
 function App() {
   return (
     <div className="App">
-      <CodeCell />
+      <TextEditor />
     </div>
   );
 }
